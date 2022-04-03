b81f5a844a4f805bc5e5ab7c92809fb306168f4c
yangee012 <77659140+yangee012@users.noreply.github.com>
2022-04-04 01:45:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.ctix2it8xu2b.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limyangee-employee"
+customregion = "us-east-1"
